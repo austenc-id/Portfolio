dba1bcaf5785d98e2b0f8dde574bc75dccbedec5
Austen Myers <austenc.id@pm.me>
2022-04-10 17:06:01 -0600

    prep for deploy

--- project/config/settings.py
+++ project/config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['acmf-dev-api.herokuapp.com']
 
 
 # Application definition
