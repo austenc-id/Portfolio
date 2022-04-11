c68c670a56832ccdb112b3e25ae620f51523f86a
Austen Myers <austenc.id@pm.me>
2022-04-10 19:38:47 -0600

    init app and connect api

---
+++ app.js
@@ -0,0 +1,40 @@
+Vue.createApp({
+    delimiters: ['--', '--'],
+    data() {
+        return {
+            title: 'Austen Myers',
+            sub_title: 'Portfolio',
+            pages: [
+                {
+                    id: 0,
+                    title: 'About',
+                    sections: []
+                },
+                {
+                    id: 1,
+                    title: 'Test',
+                    sections: []
+                },
+            ],
+            active_page: {},
+            active_section: ''
+        }
+    },
+    methods: {
+        async renderPage(page) {
+            this.active_page = this.pages[page.id]
+            this.active_section = ''
+            let url = 'https://acmf-dev.herokuapp.com/api/'
+            if (this.active_page.id === 0) {
+                await fetch(url + 'stories')
+                    .then(response => response.json())
+                    .then(data => (this.active_page.sections = data.results))
+            }
+            console.log(this.active_page)
+        },
+        renderSection(section) {
+            this.active_section = section
+        }
+    }
+
+}).mount('#app')
