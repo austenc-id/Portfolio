ab68ff3730f8254499fc491fef64f8334bcef062
Austen Myers <austenc.id@pm.me>
2022-04-23 14:16:03 -0600

    tweak responsive styling

--- server/project/config/settings.py
+++ server/project/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d*4e+f38(rmtxs-0_)9nv#0or48n66ajs+un_q!umlz@c^8)yd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['acmf-dev.herokuapp.com']
+ALLOWED_HOSTS = ['acmf-dev.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
