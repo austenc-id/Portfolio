fee7466685e6bcc8c1a42aa2894f3ffabec6d1b9
Austen Myers <austenc.id@pm.me>
2022-04-24 17:02:07 -0600

    undo conditional rendering

--- client/src/components/Chapter/Story.vue
+++ client/src/components/Chapter/Story.vue
@@ -4,14 +4,9 @@
             {{ story.title }}
         </h3>
         <div class="content">
-            <div v-for="paragraph in story.paragraphs">
-            <p v-if="paragraph.type === 'paragraph'">
+            <p v-for="paragraph in story.paragraphs">
                 {{ paragraph.content }}
             </p>
-            <ul v-if="paragraph.type === 'list'">
-            {{ paragraph.content }}
-            </ul>
-            </div>
         </div>
     </div>
 </template>
